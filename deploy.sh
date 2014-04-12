3f8572abf819787f2f6de30c328f4b8faa5260ae
Scott Willeke <scott@willeke.com>
2014-04-12 13:27:14 -0700

    cleanup, fixes to hgrc and deploy.

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ DATE=`date "+%Y-%m-%dT%H_%M_%S"`
 function cpsafe() {
  SRC=$1
  DST=$2
- if [[ -f "$DST" ]]; then
-  mv -v "$DST" "$DST.old-$DATE"
- fi
- cp -v "$SRC" "$DST" 
+ echo "Copying '$SRC' to $DST"
+ [[ -f "$SRC" ]] || echo "$SRC doesn't exist!" && return 1
+ [[ -f "$DST" ]] && mv "$DST" "$DST.old-$DATE"
+ cp "$SRC" "$DST"
 }
 
 cpsafe ./.bashrc ~/.bashrc
 cpsafe ./.bash_profile ~/.bash_profile
 cpsafe ./.gitconfig ~/.gitconfig
-#cpsafe ~/.hgconfig ~/.hgconfig
+cpsafe ./.hgrc ~/.hgrc
 
