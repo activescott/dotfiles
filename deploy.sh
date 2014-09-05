e3a9018d39f80511dc0fdbe2ba254e92c9a1c329
Scott Willeke <scott@willeke.com>
2014-09-04 21:27:37 -0700

    deploy links instead of copies

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,17 @@ DATE=`date "+%Y-%m-%dT%H_%M_%S"`
 function cpsafe() {
  SRC=$1
  DST=$2
- printf "Copying '$SRC' to $DST..."
  if [[ -f "$SRC" ]]; then 
-  [[ -f "$DST" ]] && mv "$DST" "$DST.old-$DATE"
-  cp "$SRC" "$DST"
-  printf " Copied.\n"
+  [[ -L "$DST" ]] && echo "removing link $DST" && rm "$DST" #rm if link
+  [[ -f "$DST" ]] && mv -v "$DST" "$DST.old-$DATE" #mv to "just in case" file
+  ln -vs "$SRC" "$DST"
  else
   printf "\n *ERROR*: $SRC doesn't exist!\n\n"
   return 1
  fi
 }
-
-cpsafe ./.bashrc ~/.bashrc
-cpsafe ./.bash_profile ~/.bash_profile
-cpsafe ./.gitconfig ~/.gitconfig
-cpsafe ./.hgrc ~/.hgrc
-
+DIR=$(cd $(dirname "$0"); pwd) #this script's directory
+cpsafe "$DIR/.bashrc" ~/.bashrc
+cpsafe "$DIR/.bash_profile" ~/.bash_profile
+cpsafe "$DIR/.gitconfig" ~/.gitconfig
+cpsafe "$DIR/.hgrc" ~/.hgrc
