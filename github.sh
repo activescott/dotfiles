78169b08393f952ef1a3bf65a2cee76c4d8ae9c2
Scott Willeke <scott@willeke.com>
2016-04-12 21:05:34 -0700

    to support for when origin isn't the first remote

--- github.sh
+++ github.sh
@@ -5,6 +5,10 @@ die () {
 	exit 1
 }
 
-git rev-parse --is-inside-work-tree &> /dev/null || die 'Not a git repository.'  
-tmp=`git remote -v | head -n1 | perl -p -e 's/^origin\s+git\@github.com\:([^\/]+)\/([^\.]+)\.git.*$/http:\/\/github.com\/$1\/$2/s'`
+git rev-parse --is-inside-work-tree &> /dev/null || die 'Not a git repository.'
+# Some notes about why this so I don't forget:
+#    `egrep ^origin` to only open github if it is an origin (default) remote and it may not be the first remote in the list
+#    ` head -n1 is after the egrep so that we only output a single github http url 
+tmp=`git remote -v | egrep ^origin | head -n1 | perl -p -e 's/^origin\s+git\@github.com\:([^\/]+)\/([^\.]+)\.git.*$/http:\/\/github.com\/$1\/$2/s'`
 open $tmp
+
